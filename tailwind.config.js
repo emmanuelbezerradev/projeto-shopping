6b0eb1159653b26d30b7e5329c1cf3d0030b9080
unknown <emmanuelpereira0471@digitalcollege.internal>
2025-04-16 17:01:18 -0300

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+            amarelo: '#e6c744',
+            cinza: '#c2c8da',
+          }
+      },
+    },
+    plugins: [],
+  }
